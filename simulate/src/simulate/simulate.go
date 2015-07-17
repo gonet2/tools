a7599ace163ce48e0fc3e81b26fba34d533b1349
luo Jun <johnnyluo586@gmail.com>
2015-07-17 15:04:34 +0800

    update

--- simulate/src/simulate/simulate.go
+++ simulate/src/simulate/simulate.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"crypto/rc4"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"math/big"
 	"math/rand"
@@ -50,9 +52,17 @@ func main() {
 
 	//get_seed_req
 	S1, M1 := dh.DHExchange()
-	K1 := dh.DHKey(S1, big.NewInt(rand.Int63()))
 	S2, M2 := dh.DHExchange()
-	K2 := dh.DHKey(S2, big.NewInt(rand.Int63()))
+	p2 := seed_info{
+		int32(M1.Int64()),
+		int32(M2.Int64()),
+	}
+	rst := send_proto(conn, Code["get_seed_req"], p2)
+	r1, _ := PKT_seed_info(rst)
+	log.Printf("result: %#v", r1)
+
+	K1 := dh.DHKey(S1, big.NewInt(int64(r1.F_client_send_seed)))
+	K2 := dh.DHKey(S2, big.NewInt(int64(r1.F_client_receive_seed)))
 	encoder, err = rc4.NewCipher([]byte(fmt.Sprintf("%v%v", SALT, K1)))
 	if err != nil {
 		log.Println(err)
@@ -63,11 +73,6 @@ func main() {
 		log.Println(err)
 		return
 	}
-	p2 := seed_info{
-		int32(M1.Int64()),
-		int32(M2.Int64()),
-	}
-	send_proto(conn, Code["get_seed_req"], p2)
 
 	KEY_EXCHANGE = true
 
@@ -98,7 +103,7 @@ func main() {
 
 }
 
-func send_proto(conn net.Conn, p int16, info interface{}) {
+func send_proto(conn net.Conn, p int16, info interface{}) (reader *packet.Packet) {
 	seqid++
 	payload := packet.Pack(p, info, nil)
 	writer := packet.Writer()
@@ -115,6 +120,27 @@ func send_proto(conn net.Conn, p int16, info interface{}) {
 	conn.Write(writer.Data())
 	log.Printf("send : %#v", writer.Data())
 	time.Sleep(time.Second)
+
+	//read
+	header := make([]byte, 2)
+	io.ReadFull(conn, header)
+	size := binary.BigEndian.Uint16(header)
+	r := make([]byte, size)
+	_, err := io.ReadFull(conn, r)
+	if err != nil {
+		log.Println(err)
+	}
+	if KEY_EXCHANGE {
+		decoder.XORKeyStream(r, r)
+	}
+	reader = packet.Reader(r)
+	b, err := reader.ReadS16()
+	if err != nil {
+		log.Println(err)
+	}
+	if _, ok := RCode[b]; !ok {
+		log.Println("unknown proto ", b)
+	}
 
 	return
 }
