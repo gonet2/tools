792ecad6c818c588ec9d54b23c2a29623be0eada
luo Jun <johnnyluo586@gmail.com>
2015-07-17 14:15:39 +0800

    update simulate

--- simulate/src/simulate/simulate.go
+++ simulate/src/simulate/simulate.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/rc4"
+	"encoding/binary"
+	"fmt"
+	"io"
 	"log"
+	"math/big"
 	"math/rand"
+	"misc/crypto/dh"
 	"misc/packet"
 	"net"
 	"os"
@@ -10,7 +16,11 @@ import (
 )
 
 var (
-	seqid = uint32(0)
+	seqid        = uint32(0)
+	encoder      *rc4.Cipher
+	decoder      *rc4.Cipher
+	KEY_EXCHANGE = false
+	SALT         = "DH"
 )
 
 const (
@@ -40,15 +50,28 @@ func main() {
 	}
 	defer conn.Close()
 
-	//heart_beat_req
-	send_proto(conn, Code["heart_beat_req"], nil)
-
 	//get_seed_req
+	S1, M1 := dh.DHExchange()
+	K1 := dh.DHKey(S1, big.NewInt(rand.Int63()))
+	S2, M2 := dh.DHExchange()
+	K2 := dh.DHKey(S2, big.NewInt(rand.Int63()))
+	encoder, err = rc4.NewCipher([]byte(fmt.Sprintf("%v%v", SALT, K1)))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	decoder, err = rc4.NewCipher([]byte(fmt.Sprintf("%v%v", SALT, K2)))
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	p2 := seed_info{
-		F_client_send_seed:    rand.Int31(),
-		F_client_receive_seed: 0,
+		int32(M1.Int64()),
+		int32(M2.Int64()),
 	}
 	send_proto(conn, Code["get_seed_req"], p2)
+
+	KEY_EXCHANGE = true
 
 	//user_login_req
 	p3 := user_login_info{
@@ -65,7 +88,10 @@ func main() {
 		F_login_ip:           "127.0.0.1",
 	}
 	send_proto(conn, Code["user_login_req"], p3)
-	
+
+	//heart_beat_req
+	send_proto(conn, Code["heart_beat_req"], nil)
+
 	//proto_ping_req
 	p1 := auto_id{
 		F_id: rand.Int31(),
@@ -74,14 +100,37 @@ func main() {
 
 }
 
-func send_proto(conn net.Conn, p int16, info interface{}) {
+func send_proto(conn net.Conn, p int16, info interface{}) (reader *packet.Packet) {
 	seqid++
 	payload := packet.Pack(p, info, nil)
 	writer := packet.Writer()
 	writer.WriteU16(uint16(len(payload)) + 4)
 	writer.WriteU32(seqid)
 	writer.WriteRawBytes(payload)
-	conn.Write(writer.Data())
-	log.Printf("%#v", writer.Data())
+	data := writer.Data()
+	log.Printf("%#v", data)
+	if KEY_EXCHANGE {
+		encoder.XORKeyStream(data, data)
+	}
+	conn.Write(data)
 	time.Sleep(time.Second)
+
+	//read
+	header := make([]byte, 2)
+	io.ReadFull(conn, header)
+	size := binary.BigEndian.Uint16(header)
+	r := make([]byte, size)
+	_, err := io.ReadFull(conn, r)
+	if err != nil {
+		log.Println(err)
+	}
+	reader = packet.Reader(r)
+	b, err := reader.ReadS16()
+	if err != nil {
+		log.Println(err)
+	}
+	if _, ok := RCode[b]; !ok {
+		log.Println("unknown proto ", b)
+	}
+	return
 }
