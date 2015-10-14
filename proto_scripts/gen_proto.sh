e2bd8f052a23ba0b71b63b9933ca096d290bbe8f
xtaci <daniel820313@gmail.com>
2015-10-14 14:24:35 +0800

    fix shell

--- proto_scripts/gen_proto.sh
+++ proto_scripts/gen_proto.sh
@@ -14,9 +14,9 @@ go get github.com/codegangsta/cli
 ## api.txt
 go run api.go --min 0 --max 1000 > $PATH_AGENT/api.go; go fmt $PATH_AGENT/api.go
 go run api.go --min 1001 --max 65535 > $PATH_GAME/api.go; go fmt $PATH_GAME/api.go
-go run api.go --min 0 --max 65535 > $PATH_CLIENT/NetApi.cs
+go run api.go --template "templates/client/api.tmpl" --min 0 --max 65535 > $PATH_CLIENT/NetApi.cs
 
 ## proto.txt
 go run proto.go > $PATH_AGENT/proto.go; go fmt $PATH_AGENT/proto.go
 go run proto.go > $PATH_GAME/proto.go; go fmt $PATH_GAME/proto.go
-go run proto.go > $PATH_CLIENT/NetProto.cs
+go run proto.go --template "templates/client/proto.tmpl" > $PATH_CLIENT/NetProto.cs
