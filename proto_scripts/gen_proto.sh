2dbce7ae3f2ae8f59e786f981e58675cf9c27bff
xtaci <daniel820313@gmail.com>
2015-10-21 21:10:17 +0800

    fix a misleading max proto number

--- proto_scripts/gen_proto.sh
+++ proto_scripts/gen_proto.sh
@@ -13,8 +13,8 @@ go get github.com/codegangsta/cli
 
 ## api.txt
 go run api.go --min 0 --max 1000 > $PATH_AGENT/api.go; go fmt $PATH_AGENT/api.go
-go run api.go --min 1001 --max 65535 > $PATH_GAME/api.go; go fmt $PATH_GAME/api.go
-go run api.go --template "templates/client/api.tmpl"  --min 0 --max 65535 > $PATH_CLIENT/NetApi.cs
+go run api.go --min 1001 --max 32767 > $PATH_GAME/api.go; go fmt $PATH_GAME/api.go
+go run api.go --template "templates/client/api.tmpl"  --min 0 --max 32767 > $PATH_CLIENT/NetApi.cs
 
 ## proto.txt
 go run proto.go > $PATH_AGENT/proto.go; go fmt $PATH_AGENT/proto.go
