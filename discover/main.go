209547c46684dc91ee8d11d92c0e1f54d9420f5c
xtaci <daniel820313@gmail.com>
2015-10-31 14:37:27 +0800

    update

--- discover/main.go
+++ discover/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
 	"log"
+	"net/http"
 	"os"
-	"text/template"
+)
+
+const (
+	raw = "https://raw.githubusercontent.com/gonet2/libs/master/services/services.go"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		return
+	resp, err := http.Get(raw)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	tmpl, err := template.New("discover").Parse(t)
+	// parser
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "", resp.Body, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f, err := os.Create("services.go")
-	if err != nil {
-		log.Fatal(err)
+	// remove Init()
+LOOP:
+	for k := range f.Decls {
+		switch f.Decls[k].(type) {
+		case *ast.FuncDecl:
+			decl := f.Decls[k].(*ast.FuncDecl)
+			if decl.Name.Name == "Init" {
+				f.Decls = append(f.Decls[:k], f.Decls[k+1:]...)
+				break LOOP
+			}
+		}
 	}
 
-	err = tmpl.Execute(f, os.Args[1:])
-	if err != nil {
-		log.Fatal(err)
-	}
-	f.Close()
+	// rewrite
+	format.Node(os.Stdout, fset, f)
 }
