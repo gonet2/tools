867ff5fa4ff8a1c22e6ccdd02d6ac63c5e0ff61a
xtaci <daniel820313@gmail.com>
2017-02-23 17:32:11 +0800

    update sh

--- clone_all.sh
+++ clone_all.sh
@@ -7,9 +7,8 @@ cd gonet2/src
 git clone git@github.com:gonet2/agent.git
 git clone git@github.com:gonet2/game.git
 git clone git@github.com:gonet2/snowflake.git
-git clone git@github.com:gonet2/chat.git
-git clone git@github.com:gonet2/rank.git
+git clone git@github.com:xtaci/chat.git
+git clone git@github.com:xtaci/rank.git
 git clone git@github.com:gonet2/geoip.git
-git clone git@github.com:gonet2/archiver.git
 git clone git@github.com:gonet2/wordfilter.git
 git clone git@github.com:gonet2/tools.git
